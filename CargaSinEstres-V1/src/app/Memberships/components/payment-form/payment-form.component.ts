ddabf68a311bfa61e08f737fde343f2864dd9bc9
Andrea O'Higgins <u20221b178@upc.edu.pe>
2023-09-25 13:46:23 -0500

    Update payment-form.component.ts

--- CargaSinEstres-V1/src/app/Memberships/components/payment-form/payment-form.component.ts
+++ CargaSinEstres-V1/src/app/Memberships/components/payment-form/payment-form.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-payment-form',
   templateUrl: './payment-form.component.html',
-  styleUrls: ['./payment-form.component.css']
+  styleUrls: ['./payment-form.component.scss']
 })
 export class PaymentFormComponent {
   nombre: string = '';
