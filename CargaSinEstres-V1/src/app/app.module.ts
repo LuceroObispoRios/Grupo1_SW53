b4cc610e179a173799ec9c4e0618178568ec5cfd
Lucero Obispo Rios <102556567+LuceroObispoRios@users.noreply.github.com>
2023-09-25 01:33:24 -0500

    Update app.module.ts

--- CargaSinEstres-V1/src/app/app.module.ts
+++ CargaSinEstres-V1/src/app/app.module.ts
@@ -1,50 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './UserManagement/components/login-form/login-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
-//COMPONENTS
-import { ClientFormComponent } from './UserManagement/components/client-form/client-form.component';
-import { CompanyFormComponent } from './UserManagement/components/company-form/company-form.component';
-import { LoginFormComponent } from './UserManagement/components/login-form/login-form.component';
-import { ClientSettingsComponent } from './UserManagement/components/client-settings/client-settings.component';
-import { CompanySettingsComponent } from './UserManagement/components/company-settings/company-settings.component';
-import { CompanyDetailComponent } from './CompanySearch/components/company-detail/company-detail.component';
-import { CompanyTableComponent } from './CompanySearch/components/company-table/company-table.component';
-
-//OTROS
 import { MaterialModule } from 'src/shared/material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { HistoryCardsComponent } from './BookingHistory/components/history-cards/history-cards.component';
-import { ChatDialogComponent } from './BookingHistory/components/chat-dialog/chat-dialog.component';
-import { PaymentFormComponent } from './Memberships/components/payment-form/payment-form.component';
-import { MembershipComponent } from './Memberships/components/membership/membership.component';
-import { LandingPageComponent } from './Public/components/landing-page/landing-page.component';
-import { FooterComponent } from './Public/components/footer/footer.component';
-import { ToolbarLandingComponent } from './Public/components/toolbar-landing/toolbar-landing.component';
 
+import { ToolbarLandingComponent } from './Public/components/toolbar-landing/toolbar-landing.component';
+import { FooterComponent } from './Public/components/footer/footer.component';
+import { LandingPageComponent } from './Public/components/landing-page/landing-page.component';
+import { ChatDialogComponent } from './BookingHistory/components/chat-dialog/chat-dialog.component';
+import { HistoryCardsComponent } from './BookingHistory/components/history-cards/history-cards.component';
+import { CompanyDetailComponent } from './CompanySearch/components/company-detail/company-detail.component';
+import { CompanyTableComponent } from './CompanySearch/components/company-table/company-table.component';
+import { MembershipComponent } from './Memberships/components/membership/membership.component';
+import { PaymentFormComponent } from './Memberships/components/payment-form/payment-form.component';
+import { ClientFormComponent } from './UserManagement/components/client-form/client-form.component';
+import { ClientSettingsComponent } from './UserManagement/components/client-settings/client-settings.component';
+import { CompanyFormComponent } from './UserManagement/components/company-form/company-form.component';
+import { CompanySettingsComponent } from './UserManagement/components/company-settings/company-settings.component';
+import { ToolbarClientComponent } from './Public/components/toolbar-client/toolbar-client.component';
+import { ToolbarCompanyComponent } from './Public/components/toolbar-company/toolbar-company.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientFormComponent,
-    CompanyFormComponent,
     LoginFormComponent,
-    ClientSettingsComponent,
-    CompanySettingsComponent,
+    ToolbarLandingComponent,
+    FooterComponent,
+    LandingPageComponent,
+    ChatDialogComponent,
+    HistoryCardsComponent,
     CompanyDetailComponent,
     CompanyTableComponent,
-    HistoryCardsComponent,
-    ChatDialogComponent,
-    PaymentFormComponent,
     MembershipComponent,
-    LandingPageComponent,
-    FooterComponent,
-    ToolbarLandingComponent
+    PaymentFormComponent,
+    ClientFormComponent,
+    ClientSettingsComponent,
+    CompanyFormComponent,
+    CompanySettingsComponent,
+    ToolbarClientComponent,
+    ToolbarCompanyComponent
   ],
   imports: [
     BrowserModule,
@@ -52,12 +52,9 @@ import { ToolbarLandingComponent } from './Public/components/toolbar-landing/too
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
-    RouterModule,
-    FormsModule,
-    ReactiveFormsModule
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
