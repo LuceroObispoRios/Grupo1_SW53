a77eed8092893355804ce7b1a6cddca8baf2f1b2
Lucero Obispo Rios <102556567+LuceroObispoRios@users.noreply.github.com>
2023-09-25 01:43:09 -0500

    Add files via upload

---
+++ CargaSinEstres-V1/src/app/Public/components/toolbar-client/toolbar-client.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-toolbar-client',
+  templateUrl: './toolbar-client.component.html',
+  styleUrls: ['./toolbar-client.component.scss']
+})
+export class ToolbarClientComponent {
+
+  constructor(private router: Router) { }
+
+  pageLanding(){
+    this.router.navigateByUrl('/landing-page');
+  }
+
+  pageHistoryCards(){
+    this.router.navigateByUrl('/history-cards');
+  }
+  pageCompanyTable(){
+    this.router.navigateByUrl('/company-table');
+  }
+  pageSettings(){
+    this.router.navigateByUrl('/client-settings/:id');
+  }
+}
