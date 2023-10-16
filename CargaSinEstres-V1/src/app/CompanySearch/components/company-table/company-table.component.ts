a2294b1edd7c836f2e13b784f847b039c359cb09
Lucero Obispo Rios <102556567+LuceroObispoRios@users.noreply.github.com>
2023-10-15 23:18:31 -0500

    Complete filters

--- CargaSinEstres-V1/src/app/CompanySearch/components/company-table/company-table.component.ts
+++ CargaSinEstres-V1/src/app/CompanySearch/components/company-table/company-table.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { CargaSinEstresDataService } from 'src/app/services/carga-sin-estres-data.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-company-table',
@@ -12,19 +13,29 @@ import { Router } from '@angular/router';
 export class CompanyTableComponent{
 
   dataSource_company = new MatTableDataSource();
-  displayedColumns: string[] = ['id', 'name', 'services', 'photo'];
+  displayedColumns: string[] = ['id', 'name', 'services', 'location', 'photo'];
 
   @ViewChild(MatPaginator, {static: true})
   paginator!: MatPaginator;
 
-  selectedServices: string[] = [];
-  servicesList: string[] = ['Transporte', 'Carga', 'Embalaje', 'Montaje', 'Desmontaje'];
   originalData: any[] = []; //contiene todas las empresas sin ningún filtro aplicado
 
-  constructor(private companyDataService: CargaSinEstresDataService, private router: Router) { 
+  userId: string = '';
+  constructor(private companyDataService: CargaSinEstresDataService, private router: Router, private route: ActivatedRoute) { 
+
+    // Obtiene el id del usuario
+    this.route.pathFromRoot[1].url.subscribe(
+      url => {
+        console.log('url: ', url);
+        this.userId = url[1].path;
+        console.log('User id:' + this.userId);
+      }
+    ); 
+
   }
 
   ngOnInit(): void {
+    this.getUserDistrict();
     this.dataSource_company.paginator = this.paginator;
     this.getAllCompanies(); 
   }
@@ -34,17 +45,24 @@ export class CompanyTableComponent{
       this.originalData = res;
       this.dataSource_company.data = res;
       this.searchBySelectedServices();
+      this.searchByLocation();
     })
   }
 
-  searchByCompanyName(event: Event){
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource_company.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource_company.paginator) {
-      this.dataSource_company.paginator.firstPage();
-    }
+  /* FILTER: SEARCH BY NAME */
+  manualCompanyName: string = ''; // Nombre de la empresa ingresado por el usuario
+
+  searchByCompanyName(){
+    this.dataSource_company.data = this.originalData.filter((company) => {
+      return company.name.toLowerCase().includes(this.manualCompanyName.toLowerCase());
+    });
   }
+
+  /* FILTER: SEARCH BY SERVICES */
+  selectedServices: string[] = [];
+  servicesList: string[] = ['Transporte', 'Carga', 'Embalaje', 'Montaje', 'Desmontaje'];
+  
 
   searchBySelectedServices() {
     const filterValue = this.dataSource_company.filter; // Almacena el valor actual del filtro
@@ -67,9 +85,44 @@ export class CompanyTableComponent{
     this.searchBySelectedServices(); 
   }
 
+
+  /* FILTER: SEARCH BY LOCATION */
+  searchMethod: string = 'noFilter'; // Opción predeterminada
+  manualLocation: string = ''; // Ubicación manual ingresada por el usuario
+  userLocation: string = ''; // Ubicación del usuario
+
+  getUserDistrict() {
+    this.companyDataService.getClientById(this.userId).subscribe(
+      (client) => {
+        this.userLocation = client.direccion;
+        console.log("Distrito del usuario:", this.userLocation);
+      },
+      (error) => {
+        console.error('Error al obtener el distrito del cliente:', error);
+      }
+    );
+  }
+
+  searchByLocation() {
+    if (this.searchMethod === 'userLocation') {
+      this.dataSource_company.data = this.originalData.filter((company) => {
+        return company.direccion.toLowerCase().includes(this.userLocation.toLowerCase());
+      });
+    } else if (this.searchMethod === 'manualLocation') {
+      this.dataSource_company.data = this.originalData.filter((company) => {
+        return company.direccion.toLowerCase().includes(this.manualLocation.toLowerCase());
+      });
+    } else {
+      // Si se selecciona "Sin filtro", no se aplica ningún filtro
+      this.dataSource_company.data = this.originalData;
+      this.searchMethod='';
+    }
+  }
+
+  /* GO TO COMPANY INFO PAGE */
   getRow(row: { id: any; }){
     console.log("Row clicked: ");
     console.log(row);
-    this.router.navigateByUrl(`/company/${row.id}`);
+    this.router.navigateByUrl(`client/${this.userId}/company/${row.id}`);
   }
 }
