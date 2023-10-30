73f4f17896e5fdd3a408c98adb32d76a778d535e
Andrea O'Higgins <u20221b178@upc.edu.pe>
2023-10-29 22:41:42 -0500

    Update ReviewServiceImpl.java

--- TB2-backend/cargaSinEstres/src/main/java/com/upc/backend/cargaSinEstres/service/impl/ReviewServiceImpl.java
+++ TB2-backend/cargaSinEstres/src/main/java/com/upc/backend/cargaSinEstres/service/impl/ReviewServiceImpl.java
@@ -1,17 +1,43 @@
-package com.upc.backend.cargaSinEstres.service.impl;
-
-import com.upc.backend.cargaSinEstres.model.Review;
-import com.upc.backend.cargaSinEstres.repository.ReviewRepository;
-import com.upc.backend.cargaSinEstres.service.ReviewService;
+package com.upc.cargasinestres.CargaSinEstres.service.Impl;
+import com.upc.cargasinestres.CargaSinEstres.exception.ValidationException;
+import com.upc.cargasinestres.CargaSinEstres.model.Review;
+import com.upc.cargasinestres.CargaSinEstres.repository.IReviewRepository;
+import com.upc.cargasinestres.CargaSinEstres.service.IReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
-public class ReviewServiceImpl implements ReviewService {
-    @Autowired
-    private ReviewRepository reviewRepository;
+@Qualifier("reviewServiceImpl")
+public class ReviewServiceImpl implements IReviewService {
 
-    public Review createReview(Review review) {
+    private final IReviewRepository reviewRepository;
+
+
+    public ReviewServiceImpl(IReviewRepository reviewRepository){
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public List<Review> getReviewsByCompanyId(Long companyId){
+        return reviewRepository.findByCompanyId(companyId);
+    }
+
+    @Override
+    public Review createReview(@RequestBody Review review){
+        validateReview(review);
         return reviewRepository.save(review);
+    }
+
+    private void validateReview(Review review){
+        if(review.getComment() == null || review.getComment().trim().isEmpty()){
+            review.setComment("...");
+        }
+        if(review.getRating() == 0){
+            throw new ValidationException("El rating es obligatorio");
+        }
     }
 }
