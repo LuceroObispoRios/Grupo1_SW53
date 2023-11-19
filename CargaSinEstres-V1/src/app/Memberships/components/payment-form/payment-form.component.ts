c96e7a49c32a3129d9ba783f63707705ec1b0cef
Javier Martin Sebastian Tasayco <112427316+JavierSebastianTasayco@users.noreply.github.com>
2023-11-19 18:40:31 -0500

    Update payment-form.component.ts

--- CargaSinEstres-V1/src/app/Memberships/components/payment-form/payment-form.component.ts
+++ CargaSinEstres-V1/src/app/Memberships/components/payment-form/payment-form.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { CargaSinEstresDataService } from 'src/app/services/carga-sin-estres-data.service';
 import { Company } from 'src/app/models/company.model';
 import { Subscription } from 'src/app/models/subscription.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-payment-form',
@@ -10,12 +12,36 @@ import { Subscription } from 'src/app/models/subscription.model';
 })
 export class PaymentFormComponent {
 
-  constructor(private dataService: CargaSinEstresDataService) { }
+  constructor(private dataService: CargaSinEstresDataService, private fb: FormBuilder ,private route: ActivatedRoute) {
+    this.subscriptionForm = this.fb.group({
+      correo: ['', Validators.required],
+      password: ['', Validators.required],
+      ruc: ['', Validators.required],
+      direccion: ['', Validators.required],
+      vigenciaSuscripcion: ['', Validators.required],
+      numeroTarjeta: ['', Validators.required],
+      CVV: ['', Validators.required],
+      fechaVencimiento: ['', Validators.required],
+    });
+
+    // Obtiene el id del usuario
+    this.route.pathFromRoot[1].url.subscribe(
+      url => {
+        this.userId = url[1].path;
+        console.log('User id:' + this.userId);
+      }
+    ); 
+  }
+
+  //para obtener el id del usuario
+  userId: string = '';
 
   //heredan los datos de un parent
   @Input() companyId: any; 
   @Input() companyName: any; 
   @Input() companyLogo: any; 
+
+  subscriptionForm: FormGroup;
 
   correo: string = '';
   contrasenia: string = '';
@@ -29,41 +55,34 @@ export class PaymentFormComponent {
   //para mostrar el mensaje de confirmacion
   confirmacionVisible: boolean = false;
   //para desactivar el boton de confirmacion (no hacer spam de subscripciones)
-  botonDesactivado: boolean = false;
+  botonDesactivado: boolean = true;
 
   //incializar la firma aleatoria
   firma: string = this.generarCodigoAleatorio();
 
   onSubmit() {
-    if (this.validarDatos()) {
       this.confirmacionVisible = true;
-      this.botonDesactivado = true;
+      //this.botonDesactivado = true;
+      this.ruc = this.subscriptionForm.value.ruc;
+      this.direccion = this.subscriptionForm.value.direccion;
+      this.vigenciaSuscripcion = this.subscriptionForm.value.vigenciaSuscripcion;
+      this.numeroTarjeta = this.subscriptionForm.value.numeroTarjeta;
+      this.correo = this.subscriptionForm.value.correo;
+      
 
       // Obtener la información de la empresa
-      this.dataService.getCompaniesForLogin(this.correo, this.contrasenia).subscribe(
-        (CompanyArray: Company[]) => {
-
-          if (CompanyArray.length > 0) {
-
-            const companyData = CompanyArray[0];
-
-            const nuevaSuscripcion: Subscription = {
-              id: undefined,
+      this.dataService.getCompanyById(this.userId).subscribe(
+        (companyResponse: any) => {
+            
+            const nuevaSuscripcion: any = {
               firma: this.firma,
-              idCompany: companyData.id,
-              subscriptionDate: new Date().toISOString(), 
-              subscriptionType: this.vigenciaSuscripcion, 
-              payment: {
-                paymentMethod: this.numeroTarjeta,
-              },
-              hiredCompany: {
-                name: companyData.name, 
-                logo: companyData.photo 
-              }
+              subscriptionType: this.subscriptionForm.value.vigenciaSuscripcion,
+              paymentMethod: this.subscriptionForm.value.numeroTarjeta,
             };
-
+            console.log("nuevaSuscripcion: " + nuevaSuscripcion + "id de cliente: "+ companyResponse.id);
+            
           // Crear la suscripción
-          this.dataService.createSubscription(nuevaSuscripcion).subscribe(
+          this.dataService.createSubscription(companyResponse.id ,nuevaSuscripcion).subscribe(
             (response) => {
               console.log('Suscripción creada con éxito', response);
             },
@@ -71,17 +90,13 @@ export class PaymentFormComponent {
               console.error('Error al crear la suscripción', error);
             }
           );
-          }
         },
         (error) => {
           console.error('Error al obtener la información de la empresa', error);
         }
       );
-    }
   }
   
-  
-
   generarCodigoAleatorio(): string {
     const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let codigo = '';
@@ -97,21 +112,6 @@ export class PaymentFormComponent {
 
   firmar() {
     this.firma = this.generarCodigoAleatorio();
-  }
-
-  validarDatos(): boolean {
-
-    if (!this.correo || !this.contrasenia || !this.ruc || !this.direccion || !this.vigenciaSuscripcion) {
-      return false;
-    }
-
-    if (!/^\d{11}$/.test(this.ruc)) return false;
-
-    if (!/^\d{16}$/.test(this.numeroTarjeta)) return false;
-
-    if (!/^\d{3}$/.test(this.CVV)) return false;
-  
-    return true;
   }
 
 
@@ -142,7 +142,5 @@ export class PaymentFormComponent {
     `;
     return contenido;
   }
-
-
 
 }
