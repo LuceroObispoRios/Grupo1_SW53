d1b969b34ee9d6cc579b08c6353d5f3074471cc6
Eric Cuevas <99770864+Ericcuevas03@users.noreply.github.com>
2023-11-19 10:25:06 -0500

    Delete TB2-backend/cargaSinEstres directory

--- TB2-backend/cargaSinEstres/src/main/java/com/upc/backend/cargaSinEstres/service/BookingHistoryService.java
+++
@@ -1,8 +0,0 @@
-package com.upc.backend.cargaSinEstres.service;
-
-import com.upc.backend.cargaSinEstres.model.BookingHistory;
-
-public interface BookingHistoryService {
-
-    public abstract BookingHistory createReservation(BookingHistory bookingHistory);
-}
